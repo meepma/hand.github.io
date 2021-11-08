c884eab7f6307afd2343ad0064fc5eeeb2a9aae8
meepma <80076185+meepma@users.noreply.github.com>
2021-11-08 18:57:29 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -14,7 +14,41 @@ function takeSnapShot(){
     })
 }
 console.log("ml5 version",ml5.version);
-imageClassifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/a5I1ZJ1M_/model.json",modelLoaded);
+imageClassifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/St7aLGN5z/model.json",modelLoaded);
 function modelLoaded(){
     console.log("modelLoaded");
+}
+function predict(){
+    img=document.getElementById("ThisIsWhereThePaperWorkGoes");
+    imageClassifier.classify(img,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+
+    }else{
+        console.log(results);
+        document.getElementById("name1").innerHTML=results[0].label;
+        document.getElementById("name2").innerHTML=results[1].label;
+        if(results[0].label=="Thumbs up"){
+            document.getElementById("emoji1").innerHTML="&#128077;";
+        }
+        if(results[0].label=="Good job!!!!"){
+            document.getElementById("emoji1").innerHTML="&#128076;";
+        }
+        if(results[0].label=="Victory!!!!!!!!"){
+            document.getElementById("emoji1").innerHTML="&#9996;";
+        }
+        
+        if(results[1].label=="Thumbs up"){
+            document.getElementById("emoji2").innerHTML="&#128077;";
+        }
+        if(results[1].label=="Good job!!!!"){
+            document.getElementById("emoji2").innerHTML="&#128076;";
+        }
+        if(results[1].label=="Victory!!!!!!!!"){
+            document.getElementById("emoji2").innerHTML="&#9996;";
+        }
+        
+    }
 }
